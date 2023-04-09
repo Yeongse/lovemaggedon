931d7bbb8dc68fda05fca211500c85e6f908d7c8
Yeongse <yeongsekm@gmail.com>
2023-04-09 23:32:59 +0900

    カメラ起動不可は実機ではないことに起因？

--- lib/cameraPage.dart
+++ lib/cameraPage.dart
@@ -4,6 +4,18 @@ import 'package:camera/camera.dart';
 
 import './members.dart';
 
+Future<CameraController> getCamera() async {
+  final cameras = await availableCameras();
+  final frontCamera = cameras.firstWhere(
+    (camera) => camera.lensDirection == CameraLensDirection.front,
+  );
+  // print(cameras);
+  print(frontCamera);
+  final cameraController = CameraController(frontCamera, ResolutionPreset.high);
+  await cameraController.initialize();
+  return cameraController;
+}
+
 class CameraPage extends StatelessWidget {
   const CameraPage({Key? key, required this.index, required this.sex})
       : super(key: key);
@@ -15,19 +27,34 @@ class CameraPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     final members = Provider.of<Members>(context);
-
     String sexString = sex == 'male' ? '男性' : '女性';
 
-    return Scaffold(
-        appBar: AppBar(),
-        body: Center(
-            child: Column(
-          children: [
-            Container(
-                alignment: Alignment.center,
-                child: Text('まずは$sexStringの$index番目の人の登録です。')),
-            Container(alignment: Alignment.center, child: Text('自撮りを登録してください。'))
-          ],
-        )));
+    return FutureBuilder(
+        future: getCamera(),
+        builder:
+            (BuildContext context, AsyncSnapshot<CameraController> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            final cameraController = snapshot.data;
+            // print(cameraController);
+            return Scaffold(
+                appBar: AppBar(),
+                body: Center(
+                    child: Column(
+                  children: [
+                    Container(
+                        alignment: Alignment.center,
+                        child: Text('まずは$sexStringの$index番目の人の登録です。')),
+                    Container(
+                        alignment: Alignment.center,
+                        child: Text('自撮りを登録してください。')),
+                    // CameraPreview(snapshot.data),
+                  ],
+                )));
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return CircularProgressIndicator();
+          }
+        });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:camera/camera.dart';
 import './homePage.dart';
 import './members.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   // カメラを初期化
+//   final cameras = await availableCameras();
+//   final firstCamera = cameras.first;
+
+//   runApp(MyApp(camera: firstCamera));
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
